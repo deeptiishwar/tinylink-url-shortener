d9ebe3e3d242c9f2c74c8d2b14c33718b0883580
Deepti Ishwar <deeptiishwar@DEEPTIs-MacBook-Air.local>
2025-11-22 01:38:26 +0530

    Initial commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 // IMPORTANT PART: SSL for Neon/Postgres
 const pool = new Pool({
